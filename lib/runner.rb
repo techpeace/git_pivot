372c8785599601cb90b32369086081ae7727f9ac
Matt Buck <techpeace@gmail.com>
2009-08-29 15:19:45 -0500

    Added support for the rest of the commands.

--- lib/runner.rb
+++ lib/runner.rb
@@ -22,7 +22,7 @@ require 'date'
 
 module GitPivot
   class Runner
-    SUB_COMMANDS = %w{current}
+    SUB_COMMANDS = %w{current work display start finish}
     
     def initialize(args)
       @argv = args
@@ -41,27 +41,59 @@ A command-line interface for Pivotal Tracker.
 Subcommands:
   current - Lists the stories that are part of the current iteration.
   work    - Lists the stories that you own.
+  display - Displays information about a specific story.
+  start   - Marks a story as started.
+  finish  - Marks a story as finished.
   
-        BANNER
+BANNER
         stop_on SUB_COMMANDS
       end
       
       cmd = @argv.shift
       cmd_opts = case cmd
-                 when "current"
-                   Trollop::options do
-                     banner "Lists the stories that are part of the current iteration."
-                   end
-                   @git_pivot.current_sprint
-                 when "work"
-                   # FIXME: This help message doesn't appear with a 'git_pivot work -h', but the 'git_pivot current -h' message does.
-                   Trollop::options do
-                     banner "Lists the stories that you own."
-                   end
-                   @git_pivot.my_work
-                 else
-                   Trollop::die "unknown subcommand #{cmd.inspect}"
-                 end
+        when "current"
+          command = :current_sprint
+        
+          Trollop::options do
+            banner "Lists the stories that are part of the current iteration."
+          end
+        when "work"
+          command = :my_work
+        
+          # FIXME: This help message doesn't appear with a 'git_pivot work -h', but the 'git_pivot current -h' message does.
+          Trollop::options do
+            banner "Lists the stories that you own."
+          end
+        when "display"
+          command = :display_story
+        
+          Trollop::options do
+            banner "Display information about a specific story."
+        
+            opt :id, "The id of the story to display.", :required => true, :type => Integer
+          end
+        when "start"
+          command = :start_story
+        
+          Trollop::options do
+            banner "Marks a specific story as started."
+        
+            opt :id, "The id of the story to start.", :required => true, :type => Integer
+          end
+        when "finish"
+          command = :finish_story
+        
+          Trollop::options do
+            banner "Marks a specific story as finished."
+        
+            opt :id, "The id of the story to finish.", :required => true, :type => Integer
+          end
+        else
+          Trollop::die "unknown subcommand #{cmd.inspect}"
+        end
+      
+      args = cmd_opts[:id] ? [command, cmd_opts[:id]] : [command]
+      @git_pivot.send(*args)
     end
   end
 end
